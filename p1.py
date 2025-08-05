4206b8db24ef06e56dd517040b5073bde05ac75b
minknatcha <sangsureechai_n@silpakorn.edu>
2025-08-05 19:28:16 +0700

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,16 +1,16 @@
-product_List = {}
+product_list = []
 
-def add_product(product_list, product_name, product_quantity):
-    if product_name in product_list:
-        product_list[product_name]  += product_quantity
-    else:
-        product_list[product_name] =  product_quantity
+def add_product(product_list):
+    name = input("Enter Product Name: ")
+    units = int(input("Enter Product units: "))
+    product_list.append([name,units])
 
 def show_product(product_list):
-    for key in product_list.keys():
-        print(key + " : " + str(product_list[key])) 
+    print('Product List:')
+    for index,item in enumerate(product_list):
+        print(f'{index+1}. Product {item[0]} - {item[1]} units')
 
-add_product(product_List, "Snack", 10)
-add_product(product_List, "Drink", 20)
-add_product(product_List, "Snack", 15)
-show_product(product_List)
+add_product(product_list)
+add_product(product_list)
+add_product(product_list)
+show_product(product_list)
